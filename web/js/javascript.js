b8a769a702bffc34d89eaea384fffd26793000d5
Yuhiro <powerofyuhiro@gmail.com>
2017-12-28 23:33:03 +0100

    web casi terminada
    Falta contenido y pulir alguna cosa

--- web/js/javascript.js
+++ web/js/javascript.js
@@ -1,35 +1,45 @@
 /* --- Slider -- */
 //Cargador de eventos al iniciar la página
-window.addEventListener('load',iniciar,false);
+window.addEventListener('load',startSlider,false);
 
-//Contador inicializado en cero
-var contador=0;
-
-function iniciar(){
-    setInterval('cambiarImg()',6000);
+// Iniciando el slider;
+function startSlider(){
+    setInterval(slide,8000);
 }
 
-function cambiarImg(){
-    var obj=document.getElementById('slider');
-    var obj2=obj.getElementsByTagName('img');
+// Imagenes que forman el slider
+var image = ["img/frame-01.jpg","img/frame-02.jpg"]
+var contSlide = 0;
 
-    if(contador==obj2.length){
-        for(var i=0; i<obj2.length; i++){
-            obj2[i].style.opacity='0';
-            contador--;
-        }
-        obj2[contador].style.opacity='1';
-    } else {
-        obj2[contador].style.opacity='1';
-        contador++
+// Cambio de imagenes
+function slide(){
+    contSlide++;
+    if (contSlide >= image.length){
+        contSlide = 0;
     }
+    document.getElementById("slider").src = image[contSlide];
 }
+/* --- /Slider --- */
 
-/* --- navigation menu --- */
+/* --- Menu de navegación --- */
 function select(num){
     deselect = document.getElementsByClassName("selected");
     for (var i = 0; i < deselect.length; i++){
         deselect[i].className = "noselected";
     }
     document.getElementsByClassName("noselected")[num].className = "selected";
+}
+/* --- /Menu de navegación --- */
+
+function showArticle(pos){
+    var tec = document.getElementById("tec");
+    // Mostrando lo seleccionado
+    tec.getElementsByClassName("visible")[1].removeAttribute("class");
+    tec.getElementsByTagName("article")[pos].setAttribute("class","visible");
+    
+    // Subrrallando la opcion clicada
+    var lista = tec.getElementsByTagName("ul")[0];
+    lista.getElementsByClassName("selected")[0].removeAttribute("class");
+    lista.getElementsByTagName("li")[pos - 1].setAttribute("class","selected");
+    
 }
